d786fd5faf52c407cbfc6286b6f8fbe980cc83fa
Ankita Saha <sahaankita348@gmail.com>
2025-05-05 11:01:02 -0400

    commit

--- app/test/test.py
+++ app/test/test.py
@@ -5,21 +5,14 @@ import random
 from flask import Flask
 app = Flask(__name__)
 
-def countdown(t): 
-    while t: 
-        mins, secs = divmod(t, 60) 
-        timer = '{:02d}:{:02d}'.format(mins, secs) 
-        print(timer, end="\r") 
-        time.sleep(1) 
-        t -= 1
-        
   
-countdown(int(60)) 
+
 
 @app.route("/")
 def test_timer():
     
-    return "No hablo queso!"
+   
+    return render_template("dashboard.html", ticks = t)
 
 
 if __name__ == "__main__":  # true if this file NOT imported
