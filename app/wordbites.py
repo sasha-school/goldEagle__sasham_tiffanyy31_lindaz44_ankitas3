fdf4a17787ebfb056b916afe5db456839d13fe43
Sasha Murokh <smurokh50@stuy.edu>
2025-05-08 10:53:04 -0400

    basic return functionality

--- app/wordbites.py
+++ app/wordbites.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def main():
     return render_template("wordbites.html")
 
+@app.route("/box_dropped", methods=["POST"]) #happens in the background and ensures that it doesnt need to refresh
+def box_dropped():
+    data = request.get_json()
+    box_number = data.get("box_number")
+    letter = data.get("letter")
+    print(f"Letter '{letter}' dropped in box number: {box_number}")
+    return jsonify({"status": "received", "box_number": box_number, "letter": letter})
 
 if __name__ == "__main__":
     app.run(debug=True)
