1cd2daad59522ab06656e12d4e2787100e0d6774
Sasha Murokh <smurokh50@stuy.edu>
2025-05-13 10:53:34 -0400

    db stuff

--- app/db_functions.py
+++ app/db_functions.py
@@ -1,21 +1,95 @@
-
 import sqlite3
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
 
-USER_FILE = "db_functions.db"
+#DB_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'database.db')
 
-def build():
-    createUsers()
+#corresponding integers
+anagrams =1
+wordhunt =2
+wordbites =3
 
-def createUsers():
-    users = sqlite3.connect(USER_FILE)
-    c = users.cursor()
-    command = "CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)"
-    c.execute(command)
-    users.commit()
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DB_DIR = os.path.join(BASE_DIR, "data")
+os.makedirs(DB_DIR, exist_ok=True)  # Make sure the folder exists
+
+DB_PATH = os.path.join(DB_DIR, 'database.db')
+print("DB_PATH:", DB_PATH)
+
+def get_db_connection():
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_tables():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS users(
+            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password_hash TEXT NOT NULL
+        );
+    ''')
+
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS anagrams_leaderboard(
+            user_id INTEGER NOT NULL,
+            games_played INTEGER NOT NULL,
+            top_score INTEGER NOT NULL
+        );
+    ''')
+
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS wordhunt_leaderboard(
+            user_id INTEGER NOT NULL,
+            games_played INTEGER NOT NULL,
+            top_score INTEGER NOT NULL
+        );
+    ''')
+
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS wordbites_leaderboard(
+            user_id INTEGER NOT NULL,
+            games_played INTEGER NOT NULL,
+            top_score INTEGER NOT NULL
+        );
+    ''')
+
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS friends(
+            user_id1 INTEGER NOT NULL,
+            user_id2 INTEGER NOT NULL
+        );
+    ''')
+
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS friends(
+            user_id1 INTEGER NOT NULL,
+            user_id2 INTEGER NOT NULL
+        );
+    ''')
+
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS challengehistory(
+            challenge_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id1 INTEGER NOT NULL,
+            user_id2 INTEGER,
+            game_name INTEGER NOT NULL,
+            winner_id INTEGER,
+            score1 INTEGER NOT NULL,
+            score2 INTEGER,
+            board TEXT NOT NULL
+        );
+    ''')
+
+    conn.commit()
+    conn.close()
 
 
 def addUser(username, password):
-    users = sqlite3.connect(USER_FILE)
+    users = sqlite3.connect(DB_PATH)
     goodcharas = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12345678910._-")
     if set(username).difference(goodcharas) or set(password).difference(goodcharas):
         return "There are special characters in the username or password."
@@ -28,7 +102,7 @@ def addUser(username, password):
 
 
 def checkPassword(username, password):
-    users = sqlite3.connect(USER_FILE)
+    users = sqlite3.connect(DB_PATH)
     c = users.cursor()
     c.execute("SELECT password FROM users WHERE username=?", (username,))
     res = c.fetchone()
