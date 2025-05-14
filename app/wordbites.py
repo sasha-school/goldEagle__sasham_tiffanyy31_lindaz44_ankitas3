3e01ce48603963162595ac7200b671475130dc30
Sasha Murokh <smurokh50@stuy.edu>
2025-05-14 10:58:16 -0400

    wordbites progress

--- app/wordbites.py
+++ app/wordbites.py
@@ -1,23 +1,33 @@
 from flask import *
 
-
+wordbites_letter_positions = {}
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    l = ['A', 'B', 'C']
-    imgs = ['https://i.pinimg.com/474x/27/90/cd/2790cd967304e6cdc4267ae9a0d67b2b.jpg', 'https://pngimg.com/d/letter_b_PNG49.png', 'https://cdn3.iconfinder.com/data/icons/letters-and-numbers-1/32/letter_C_blue-512.png']
-    letters = list(zip(l, imgs))
+    global wordbites_letter_positions
+    wordbites_letter_positions = {} #reset letter positions (for every game)
+    #future: select 5 random letters from all
+    letters = ['A', 'B', 'C']
+
+    for i, letter in enumerate(letters):
+        if letter not in wordbites_letter_positions:
+            wordbites_letter_positions[letter] = i + 1
+    print(wordbites_letter_positions)
+
     return render_template("wordbites.html", letters = letters)
 
-@app.route("/box_dropped", methods=["POST"]) #happens in the background and ensures that it doesnt need to refresh
-def box_dropped():
+@app.route("/wordbites_helper", methods=["POST"]) #happens in the background and ensures that it doesnt need to refresh
+def wordbites_helper():
     data = request.get_json()
     letter = data.get("letter")
     from_box = data.get("from_box")
     to_box = data.get("to_box")
 
     print(f"Letter '{letter}' moved from box {from_box} to box {to_box}")
+    wordbites_letter_positions[letter] = to_box
+    print(wordbites_letter_positions)
+    #future: check for words
     return jsonify({"status": "received"})
 
 if __name__ == "__main__":
