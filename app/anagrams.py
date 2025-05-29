96e37ae6732b6291d1094a942dcf5c2342f8ad4a
Ankita Saha <sahaankita348@gmail.com>
2025-05-28 23:37:34 -0400

    timer fix

--- app/anagrams.py
+++ app/anagrams.py
@@ -1,14 +1,30 @@
 import random
-
+import csv
 
 def getWordSelectionAnagrams():
     vowels = "aeiou"
     vowels = list(vowels)
     cons = "qwrtypsdfghjklzxcvbnm"
     cons = list(cons)
+    bigV = []
+    bigC = []
+
+    try:
+        with open('letters_w.csv', 'r') as file:
+            csv_r = csv.reader(file)
+            for row in csv_r:
+                if row[0].lower() in vowels:
+                    bigV.extend(row)
+                else: 
+                    bigC.extend(row)
+                print(row)
+   
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
     
-    vowelSet = random.sample(vowels, 3)
-    conSet = random.sample(cons, 3)
+    vowelSet = random.sample(bigV, 3)
+    conSet = random.sample(bigC, 3)
 
     letterSet = vowelSet + conSet
     random.shuffle(letterSet)
