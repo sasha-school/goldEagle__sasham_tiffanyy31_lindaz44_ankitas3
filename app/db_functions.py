34ce598c5603be9b458f2afa80a536a70f3fea79
Sasha Murokh <smurokh50@stuy.edu>
2025-06-01 23:12:26 -0400

    error fix

--- app/db_functions.py
+++ app/db_functions.py
@@ -8,7 +8,7 @@ anagrams = 1
 wordhunt = 2
 wordbites = 3
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 DB_DIR = os.path.join(BASE_DIR, "data")
 os.makedirs(DB_DIR, exist_ok=True)  # Make sure the folder exists
 
